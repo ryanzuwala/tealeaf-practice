082abbc4f78c73c80c2faef532ceabba23716538
Ryan Zuwala <ryanzuwala@gmail.com>
2015-06-27 21:58:28 -0700

    complete exercise 3

--- basics/exercise3.rb
+++ basics/exercise3.rb
@@ -1,1 +1,10 @@
-nul is ./nul
+# Write a program that uses a hash to store a list of movie titles with the year they came out.
+# Then use the puts command to make your program print out the year of each movie to the screen.
+
+movie_hash = {:die_hard => 1985, :shrek => 2001, :shrek2 => 2003, :shrek3 => 2005, :fast5 => 2013}
+
+puts movie_hash[:die_hard]
+puts movie_hash[:shrek]
+puts movie_hash[:shrek2]
+puts movie_hash[:shrek3]
+puts movie_hash[:fast5]
