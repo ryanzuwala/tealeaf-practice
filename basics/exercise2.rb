7292d560eedd6645da18f9c23d37952fa266cba9
Ryan Zuwala <ryanzuwala@gmail.com>
2015-06-27 21:48:09 -0700

    add exercise 3

--- basics/exercise2.rb
+++ basics/exercise2.rb
@@ -1,14 +1,14 @@
- # Use the modulo operator, division, or a combination of both to take a 4 digit number and find
- # 1) the thousands number
- # 2) the hundreds
- # 3) the tens and
- # 4) and the ones.
+# Use the modulo operator, division, or a combination of both to take a 4 digit number and find
+# 1) the thousands number
+# 2) the hundreds
+# 3) the tens and
+# 4) and the ones.
 
- number = 6981
+number = 6981
 
- thousands = number / 1000
- hundreds = (number % 1000) / 100
- tens = (number % 100) / 10
- ones = number % 10
+thousands = number / 1000
+hundreds = (number % 1000) / 100
+tens = (number % 100) / 10
+ones = number % 10
 
- print "thousands: #{thousands}\nhundreds: #{hundreds}\ntens: #{tens}\nones: #{ones}"
+print "thousands: #{thousands}\nhundreds: #{hundreds}\ntens: #{tens}\nones: #{ones}"
