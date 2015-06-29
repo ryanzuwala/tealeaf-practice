2fd138c0563652f10a7fdd15d9ad2b0618e2b31f
Ryan Zuwala <ryanzuwala@gmail.com>
2015-06-28 21:23:52 -0400

    modified name.rb as per exercises 3 and 4

--- variables/name.rb
+++ variables/name.rb
@@ -1,6 +1,6 @@
-print 'What is your name? '
-name = gets.chomp
+print 'What is your first name? '
+first_name = gets.chomp
+print 'What is your last name? '
+last_name = gets.chomp
 
-10.times do |n|
-  puts name
-end
+puts "#{first_name} #{last_name}"
