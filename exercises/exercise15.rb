ba24c740202ef0656e3a95331df223e5c1a81678
Ryan Zuwala <ryanzuwala@gmail.com>
2015-07-03 17:47:43 -0700

    exercise 15 complete

--- exercises/exercise15.rb
+++ exercises/exercise15.rb
@@ -4,3 +4,12 @@
 arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
 
 # Then recreate the arr and get rid of all of the words that start with "s" or starts with "w".
+
+arr.delete_if { |word| word.start_with? 's' }
+
+p arr
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+arr.delete_if { |word| word.start_with?('s', 'w') }
+
+p arr
